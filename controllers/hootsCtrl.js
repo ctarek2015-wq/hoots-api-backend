269ce9e1c44216ceff0214f93c381a65ea51d184
ahmed-tarek <c.tarek2015@gmail.com>
2026-09-10 11:13:38 +0300

    feat: update hoots index to populate comments author in response

--- controllers/hootsCtrl.js
+++ controllers/hootsCtrl.js
@@ -16,7 +16,7 @@ const create = async (req, res) => {
 const index = async (req, res) => {
   try {
     const hoots = await Hoot.find()
-      .populate("author")
+      .populate("author", "comments.author")
       .sort({ createdAt: "desc" });
     res.status(200).json(hoots);
   } catch (error) {
